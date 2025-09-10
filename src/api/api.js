3df028cc4cebc82d189f0e3f2dd213e43dc97eaa
jarasalauatdinov <jarasalawatdinov111@gmail.com>
2025-09-10 22:41:04 +0500

    jaras

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cbefab3da55c.ngrok-free.app/api/v1",
+  baseURL: "https://discounts-poker-studio-runtime.trycloudflare.com/api/v1",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
